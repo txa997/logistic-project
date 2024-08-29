d94fe7bf9bf5fde320aae175e3800a1653911c07
akmol hosen <akmolhosen997@gmail.com>
2024-08-29 18:53:18 +0600

    h1-20%

---
+++ logistic-html/assets/js/main.js
@@ -0,0 +1,53 @@
+// preloader
+document.addEventListener("DOMContentLoaded", function () {
+	window.addEventListener('load', function(){
+		setTimeout(function() {
+
+			// hero-slider
+			if($('.laa-hero-1-active').length) {
+				let slider = new Swiper('.laa-hero-1-active', {
+					loop: true,
+					spaceBetween: 0,
+					speed: 500,
+					autoplay: {
+						delay: 4000000,
+					},
+
+                    effect: "fade",
+					fadeEffect: {
+						crossFade: true
+					},
+			
+					navigation: {
+						nextEl: ".laa-h1-slider-next",
+						prevEl: ".laa-h1-slider-prev",
+					},
+			
+				});
+			}
+
+
+		}, 500);
+
+	})
+
+});
+
+
+// hero-slider
+if($('.laa-s1-active').length) {
+	let slider = new Swiper('.laa-s1-active', {
+		loop: true,
+		spaceBetween: 0,
+		speed: 500,
+		autoplay: {
+			delay: 4000000,
+		},
+
+		effect: "fade",
+		fadeEffect: {
+			crossFade: true
+		},
+
+	});
+}
