3b1745d95636f579c16872a54c3cf757eba46ac1
akmol hosen <80000366+txa997@users.noreply.github.com>
2024-08-31 00:51:49 +0600

    transtion add

--- logistic-html/assets/js/main.js
+++ logistic-html/assets/js/main.js
@@ -39,15 +39,15 @@ if($('.laa-s1-active').length) {
 	let slider = new Swiper('.laa-s1-active', {
 		loop: true,
 		spaceBetween: 0,
-		speed: 500,
+		speed: 1000,
 		autoplay: {
-			delay: 4000000,
+			delay: 5000000,
 		},
 
-		effect: "fade",
-		fadeEffect: {
-			crossFade: true
-		},
+		// effect: "fade",
+		// fadeEffect: {
+		// 	crossFade: true
+		// },
 
 	});
 }
